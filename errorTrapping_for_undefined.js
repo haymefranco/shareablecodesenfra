6b31ed3e42d17c4213ebb63b90fde8f162903cb8
cr54.franclin Cebu <cr54.franclin@enfrausaph.com>
2019-02-16 07:57:47 +0800

    EOD UPDATE / ADD NEW FILES

--- errorTrapping_for_undefined.js
+++ errorTrapping_for_undefined.js
@@ -1,13 +1,13 @@
-    setInterval(function(){
-        var x = $('.classinit').attr('class');
-        if (x === undefined){
-        	//$( append script here)
-        	console.log(x);
-        }
-        else {
-            return;
-        }
-    },500);
+setInterval(function(){
+    var x = $('.classinit').attr('class');
+    if (x === undefined){
+    	//$( append script here)
+    	console.log(x);
+    }
+    else {
+        return;
+    }
+},500);
 
 
 
@@ -16,14 +16,14 @@
 
 
 
-    //==============INTERVALSTOPPER VALUE is ZERO================
-    var x = 0;
-    setInterval(function(){
-        if (x === 0){
-        (CODE HERE)
-        x++;
-        }
-        else{
-            return;
-        }
-    },500);
+//==============INTERVALSTOPPER VALUE is ZERO================
+var x = 0;
+setInterval(function(){
+    if (x === 0){
+    (CODE HERE)
+    x++;
+    }
+    else{
+        return;
+    }
+},500);
